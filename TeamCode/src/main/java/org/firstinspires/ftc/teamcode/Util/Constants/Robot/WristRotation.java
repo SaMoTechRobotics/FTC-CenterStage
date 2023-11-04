c3af52e56612cf21f2d964aded9cb28b212284a3
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-11-04 16:17:59 -0700

    Tweaked rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
@@ -6,13 +6,13 @@ import com.acmerobotics.dashboard.config.Config;
 public class WristRotation {
     public static Double ArmComplementAngle = 90.0;
 
-    public static Double PositionAt0Degrees = 0.2;
+    public static Double PositionAt0Degrees = 0.35;
     public static Double PositionAt180Degrees = 0.85;
 
     public static Double HoldDown = 0.93;
     public static Double Down = 0.9;
     public static Double Up = 0.2;
-    public static Double Forward = 0.5;
+    public static Double Forward = 0.6;
 
     public static Double Hang = 0.93;
 
