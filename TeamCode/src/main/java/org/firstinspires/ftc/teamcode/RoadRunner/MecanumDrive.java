d00ebd5482c9a3fcc3f33a28bbb30c11ff7eca9b
BrandonKirbyson <brandonkirbyson@gmail.com>
2023-11-22 13:44:28 -0800

    Tuning done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/MecanumDrive.java
@@ -28,7 +28,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 0.637733289776234;
         public double kV = 0.00015007233513335272;
-        public double kA = 0;
+        public double kA = 0.00002;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
