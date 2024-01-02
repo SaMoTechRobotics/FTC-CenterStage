5d29621f93567984bd258dc87547821105a0f62e
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-01-02 15:07:26 -0800

    Adjusted wrist rotation for board delivery

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
@@ -4,7 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class WristRotation {
-    public static Double ArmComplementAngle = 0.0; // 105.0
+    public static Double ArmComplementAngle = 10.0; // 105.0
     public static Double PickupComplementAngle = 180.0;
 
     public static Double PositionAt0Degrees = 0.3;
