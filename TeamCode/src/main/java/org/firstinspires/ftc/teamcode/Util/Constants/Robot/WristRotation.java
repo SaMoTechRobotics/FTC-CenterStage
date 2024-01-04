33d2d19763fdaf103bc2c01b6f0d5fcb823f7b28
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-01-04 13:53:53 -0800

    New axon servo with diff range

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
@@ -4,18 +4,18 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class WristRotation {
-    public static Double ArmComplementAngle = 10.0; // 105.0
+    public static Double ArmComplementAngle = -5.0; // 105.0
     public static Double PickupComplementAngle = 180.0;
 
     public static Double PositionAt0Degrees = 0.3;
     public static Double PositionAt180Degrees = 1.0;
 
     public static Double HoldDown = 0.0;
-    public static Double Down = 0.0;
+    public static Double Down = 0.15;
     public static Double Up = 1.0;
     public static Double Forward = 0.4;
 
-    public static Double Hang = 0.7;
-    public static Double HangLock = 0.15;
+    public static Double Hang = 0.0;
+    public static Double HangLock = 0.2;
 
 }
