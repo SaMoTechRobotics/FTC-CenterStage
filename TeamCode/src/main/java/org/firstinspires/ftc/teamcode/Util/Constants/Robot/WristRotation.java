fd2bb78a69e922756a444bd49b3187656883fb7b
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-01-04 17:32:14 -0800

    Hold down rotation fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/Robot/WristRotation.java
@@ -4,13 +4,13 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class WristRotation {
-    public static Double ArmComplementAngle = -5.0; // 105.0
+    public static Double ArmComplementAngle = 0.0; // 105.0
     public static Double PickupComplementAngle = 180.0;
 
     public static Double PositionAt0Degrees = 0.3;
     public static Double PositionAt180Degrees = 1.0;
 
-    public static Double HoldDown = 0.0;
+    public static Double HoldDown = 0.15;
     public static Double Down = 0.15;
     public static Double Up = 1.0;
     public static Double Forward = 0.4;
