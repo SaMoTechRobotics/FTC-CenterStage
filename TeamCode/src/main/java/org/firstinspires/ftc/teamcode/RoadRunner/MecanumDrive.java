ee3ee0554bf42dffc4b2b80112415c8ad42e2a00
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-01-21 11:54:07 -0800

    Removed imu to maybe help disconnections

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/MecanumDrive.java
@@ -9,7 +9,6 @@ import com.acmerobotics.roadrunner.Actions;
 import com.acmerobotics.roadrunner.*;
 import com.acmerobotics.roadrunner.ftc.*;
 import com.qualcomm.hardware.lynx.LynxModule;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.*;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
@@ -175,11 +174,13 @@ public final class MecanumDrive {
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        imu = hardwareMap.get(IMU.class, "imu");
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-        imu.initialize(parameters);
+//        imu = hardwareMap.get(IMU.class, "imu");
+//        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+//                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+//                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+//        imu.initialize(parameters);
+
+        imu = null;
 
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
