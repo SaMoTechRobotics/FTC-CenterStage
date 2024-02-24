2b1ff329f2fab57a49b7ebb489041d68cfe172de
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-02-24 15:12:52 -0800

    Disabled old autos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueNear.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.util.auto.AutoColor;
 import org.firstinspires.ftc.teamcode.util.auto.AutoSide;
 
 @Autonomous(name = "Auto: Blue Near", group = "Autonomous")
+@Disabled
 public class AutoBlueNear extends AutoBase {
     @Override
     protected AutoSide getSide() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OldAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OldAutoBlue.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.util.auto.AutoColor;
 import org.firstinspires.ftc.teamcode.util.auto.AutoSide;
 
 @Autonomous(name = "AutoBlue")
+@Disabled
 public class OldAutoBlue extends OldBaseAuto {
     @Override
     public void setConstants() {
