ac14221a8536a1e988866c73057bea28545bb545
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-02-25 08:34:15 -0800

    Moved to old

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldAutoRed.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.auto.old;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldBaseAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldBaseAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.auto.old;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.*;
