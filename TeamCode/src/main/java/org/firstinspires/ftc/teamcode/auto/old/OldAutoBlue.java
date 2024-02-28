43f7c00ba801fd4743a18abe4d1644b4e777b5af
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-02-28 12:59:59 -0800

    Bye old auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldAutoBlue.java
@@ -1,16 +1,11 @@
 package org.firstinspires.ftc.teamcode.auto.old;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import org.firstinspires.ftc.teamcode.util.auto.AutoColor;
-import org.firstinspires.ftc.teamcode.util.auto.AutoSide;
-
-@Autonomous(name = "AutoBlue")
-@Disabled
-public class OldAutoBlue extends OldBaseAuto {
-    @Override
-    public void setConstants() {
-        SIDE = AutoSide.FAR;
-        COLOR = AutoColor.BLUE;
-    }
-}
+//@Autonomous(name = "AutoBlue")
+//@Disabled
+//public class OldAutoBlue extends OldBaseAuto {
+//    @Override
+//    public void setConstants() {
+//        SIDE = AutoSide.FAR;
+//        COLOR = AutoColor.BLUE;
+//    }
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/old/OldAutoRed.java
@@ -1,16 +1,11 @@
 package org.firstinspires.ftc.teamcode.auto.old;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import org.firstinspires.ftc.teamcode.util.auto.AutoColor;
-import org.firstinspires.ftc.teamcode.util.auto.AutoSide;
-
-@Autonomous(name = "AutoRed")
-@Disabled
-public class OldAutoRed extends OldBaseAuto {
-    @Override
-    public void setConstants() {
-        SIDE = AutoSide.FAR;
-        COLOR = AutoColor.RED;
-    }
-}
+//@Autonomous(name = "AutoRed")
+//@Disabled
+//public class OldAutoRed extends OldBaseAuto {
+//    @Override
+//    public void setConstants() {
+//        SIDE = AutoSide.FAR;
+//        COLOR = AutoColor.RED;
+//    }
+//}
