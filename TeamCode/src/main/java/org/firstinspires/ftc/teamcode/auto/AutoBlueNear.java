488862a3c86df01783efdf360247b318ee4ef000
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-02-29 17:59:42 -0800

    Enabled auto nears

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueNear.java
@@ -1,12 +1,10 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.util.auto.AutoColor;
 import org.firstinspires.ftc.teamcode.util.auto.AutoSide;
 
 @Autonomous(name = "Auto: Blue Near", group = "Autonomous")
-@Disabled
 public class AutoBlueNear extends AutoBase {
     public AutoBlueNear() {
         super(AutoColor.BLUE, AutoSide.NEAR);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRedNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoRedNear.java
@@ -1,12 +1,10 @@
 package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.teamcode.util.auto.AutoColor;
 import org.firstinspires.ftc.teamcode.util.auto.AutoSide;
 
 @Autonomous(name = "Auto: Red Near", group = "Autonomous")
-@Disabled
 public class AutoRedNear extends AutoBase {
     public AutoRedNear() {
         super(AutoColor.RED, AutoSide.NEAR);
